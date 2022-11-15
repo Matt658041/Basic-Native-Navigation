f399e5f0915b52e09c1a06e128676016d42a5ce2
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-15 09:48:21 -0600

    added uselayout to render earlier

--- App.js
+++ App.js
@@ -10,11 +10,33 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            //this is one way to dynamically render id titles in header
+            //options={({ route, navigation }) => {
+              //const catId = route.params.categoryId;
+              //return {
+               // title: catId
+              //};
+            //}}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,10 @@
 import { View, FlatList, StyleSheet, Text } from "react-native";
-import { MEALS } from "../data/dummy-data";
-import React from "react";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+import React, { useLayoutEffect } from "react";
 import MealItem from "../components/MealItem";
 //import { useRoute } from '@react-navigation/native';
 
-export default function MealsOverviewScreen({ route }) {
+export default function MealsOverviewScreen({ route, navigation }) {
   //const route = useRoute(); this is an alternative hook you can use for routing in a nested component
   //route.params
   const catId = route.params.categoryId;
@@ -13,10 +13,27 @@ export default function MealsOverviewScreen({ route }) {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
 
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
   function renderMealItem(itemData) {
-    return (
-      <MealItem title={itemData.item.title} imageUrl={itemData.item.imageUrl} />
-    );
+    const item = itemData.item;
+
+    const mealtItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealtItemProps} />;
   }
 
   return (
