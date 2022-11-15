5efdee8adaa22daf0553966659570f86402bdcfc
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-15 14:38:55 -0600

    added meal detail folder with list and subtitle

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
@@ -9,21 +9,56 @@ export default function MealDetailScreen({ route }) {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View>
-      <Image source={{ uri: selectedMeal.imageUrl }} />
-      <Text>{selectedMeal.title}</Text>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealDetails
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredients</Text>
+      <View style={styles.subtitleContainer}>
+        <Text style={styles.subtitle}>Ingredients</Text>
+      </View>
       {selectedMeal.ingredients.map((ingredient) => (
         <Text key={ingredient}>{ingredient}</Text>
       ))}
-      <Text>Steps</Text>
+           <View style={styles.subtitleContainer}>
+              <Text style={styles.subtitle}>Steps</Text>
+              </View>
       {selectedMeal.steps.map((step) => (
         <Text key={step}>{step}</Text>
       ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 180,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  subtitle: {
+    color: "#e2b497",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subtitleContainer: {
+    borderBottomColor: "white",
+    borderBottomWidth: 2,
+    padding: 6,
+    marginHorizontal: 24,
+    marginVertical: 4,
+  },
+});
